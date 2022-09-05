cc8bdd8523950d55fc2574e4a0290179cca64996
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-05 19:06:55 +0100

    strings formatting

---
+++ strings/code.py
@@ -0,0 +1,17 @@
+# name = 'Bayo'
+# greeting = f'Bawo ni, {name}'
+
+# print(greeting)
+
+# name = 'Tunde'
+
+# greeting = f"Bawo ni, {name}"
+
+# print(greeting)
+
+
+name = 'Kunle'
+greeting = 'Hello, {}'
+with_name = greeting.format(name)
+
+print(with_name)
