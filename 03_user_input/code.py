c37c59de65f1456e905643fbf1bedfe7215d7d02
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-06 00:50:40 +0100

    user input

---
+++ 03_user_input/code.py
@@ -0,0 +1,13 @@
+# name = input('Enter your name? ')
+
+# print(name)
+
+# size_input = input('How big is your house (in square feet): ')
+# square_feet = int(size_input)
+# square_metres = square_feet/3.8
+# print(f'{square_feet} square feet is {square_metres:.2f} square metres.')
+
+
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+print(f'Your age, {user_age}, is equal to {months} months.')
