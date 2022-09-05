6fd41ded268ec2dd5f3a4f371c5aa61e30d237d2
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-05 22:20:58 +0100

    string formatting 2

--- 02_strings/code.py
+++ 02_strings/code.py
@@ -10,8 +10,14 @@
 # print(greeting)
 
 
-name = 'Kunle'
-greeting = 'Hello, {}'
-with_name = greeting.format(name)
+# name = 'Kunle'
+# greeting = 'Hello, {}'
+# with_name = greeting.format(name)
 
-print(with_name)
+# print(with_name)
+
+longer_phrase = 'Hello, {}. Today is {}.'
+
+formatted_phrase = longer_phrase.format('Tunde', 'Thursday')
+
+print(formatted_phrase)
