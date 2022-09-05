0de6d8bb2ce535d6da18276b1999b161ab686026
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-05 18:52:51 +0100

    variables

---
+++ variables/code.py
@@ -0,0 +1,30 @@
+# x = 12
+
+# price = 9.5
+
+# discount = 0.4
+
+# result = price * (1 - discount)
+
+# print(result)
+
+
+# name = 'Taiwo'
+
+# print(name)
+
+
+from re import A
+
+
+a = 20
+
+b = a
+
+print(a)
+print(b)
+
+b = 15
+
+print(a)
+print(b)
