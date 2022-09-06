59716fde2532a5e430b36ca46451c95a4809fb31
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-06 10:22:46 +0100

    list, tuples and sets

---
+++ 05_lists_tuples_sets/code.py
@@ -0,0 +1,27 @@
+l = ['Wale', 'Tunde', 'Kunle']
+t = ('Wale', 'Tunde', 'Kunle')
+s = {'Wale', 'Tunde', 'Kunle'}
+
+# l.append('Bayo')
+
+# l.remove('Kunle')
+
+s.add('Bisi')
+s.add('Bisi')
+# Features of a list
+# A list can be edit and changed. A list has the feature append or remove
+
+# Features of Tuple
+
+# A tuple cannot be edited or changed, hence, it does not have the append or remove features
+l.append('Bayo')
+# Features of a Set
+
+# A set does not allow for the subscript notation.
+# A set does not have an order
+# There can be no duplicate element in a set
+
+
+print(t)
+print(l)
+print(s)
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Python training and refresher course
 
 _Learning about variables._
+
+_Learning about string_
+
+_Learning about user inputs_
