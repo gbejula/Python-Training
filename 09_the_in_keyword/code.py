b5eb487ba58107facbe416b28798966f12d5f3d9
Olugbenga Ajayi <gbejula@gmail.com>
2022-09-26 23:53:07 +0100

    in keyword

---
+++ 09_the_in_keyword/code.py
@@ -0,0 +1,9 @@
+# friends = ['Ronke', 'Bisi', 'Tunde']
+# print('Tunde' in friends)
+
+# Answer = True
+
+movies_watched = ['The Matrix', 'Green Book', 'Her']
+user_movie = input("Enter something you've watched recently: ")
+
+print(user_movie in movies_watched)
