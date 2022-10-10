99b7a7eecf1b2b503b32b4ec5f6a6429f75f1c1e
Olugbenga Ajayi <gbejula@gmail.com>
2022-10-10 09:52:54 +0100

    conditionals1

---
+++ #100daysofcode/rollercoaster.py
@@ -0,0 +1,13 @@
+print("Welcome to the Roller Coaster Ride!!!")
+height = int(input("What is your height: "))
+if (height > 120):
+    print("You can take the ride")
+    age = int(input("What is your age: "))
+    if age < 12:
+        print("Child ticket is $5")
+    elif age <= 18:
+        print("Tennager ticket is $7")
+    else:
+        print("Adult ticket is $12")
+else:
+    print("Sorry you are too young to take the ride.")
