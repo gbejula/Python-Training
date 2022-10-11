1c54c1f1aee19e9d89cb28a0cf06c404c2f0567a
Olugbenga Ajayi <gbejula@gmail.com>
2022-10-11 23:33:45 +0100

    correct pizza code

--- #100daysofcode/pizza.py
+++ #100daysofcode/pizza.py
@@ -2,18 +2,24 @@ print("Welcome to the Python Pizza Deliveries!")
 
 
 size = input("What size of pizza do you want? S, M, L: ")
-# add_pepperoni = input("Do you want pepperoni? Y or N: ")
-# extra_cheese = input("Do you want extra cheese? Y or N: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
 bill = 0
 
-if (size == "S"):
+if size == "S":
     bill += 15
-    print(f"The price of your pizza is: {bill}")
-elif (size == "M"):
+elif size == "M":
     bill += 20
-    print(f"The price of your pizza is: {bill}")
-elif (size == "L"):
-    bill += 25
-    print(f"The price of your pizza is: {bill}")
 else:
-    print("Sorry, you selected the wrong input.")
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"The price of your pizza is {bill}")
